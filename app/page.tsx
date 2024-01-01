5b26c5d2181409126b31c29b61d509627fcf04bb
Billy Schweigert <billyschweigert@gmail.com>
2024-01-01 15:54:44 -0500

    making the analytics id a public env variable

--- app/page.tsx
+++ app/page.tsx
@@ -52,7 +52,7 @@ export default function Home() {
         </div>
       )}
       <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_MEASUREMENT_ID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
       />
       <Script id="google-analytics">
         {`
@@ -60,7 +60,7 @@ export default function Home() {
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
  
-          gtag('config', ${process.env.GA_MEASUREMENT_ID}');
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
         `}
       </Script>
     </main>
